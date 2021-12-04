af23ebec6ce9d12456e5141bd974d90d12da8f62
Hubertius <hubert.kopec@interia.pl>
2021-12-04 22:11:26 +0100

    SpeedConverter

---
+++ Section_4/SpeedConverter/src/pl/samouczekprogramisty/kursjava/Main.java
@@ -0,0 +1,28 @@
+package pl.samouczekprogramisty.kursjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        printConversion(0.0);
+        printConversion(2.5);
+        printConversion(7.1);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if(kilometersPerHour < 0.0) {
+            return -1L;
+        }
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if(kilometersPerHour < 0.0) {
+            System.out.println("Invalid Value!");
+            return;
+        }
+        System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour));
+    }
+
+
+}
