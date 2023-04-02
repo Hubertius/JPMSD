1f71e6fd1be1f9124e339401eb81f095492cb1b6
Hubertius <hubert.kopec@interia.pl>
2023-04-02 23:23:34 +0200

    Mappable: Generics challange

---
+++ Section_12-Generics/Mappable/src/Park.java
@@ -0,0 +1,14 @@
+public class Park extends Point {
+
+    private final String name;
+
+    public Park(String name, String location) {
+        super(location);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " national park";
+    }
+}
